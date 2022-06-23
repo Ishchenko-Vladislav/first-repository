0f05e1aa99ed31d0b98ca7fac4c99c40c7b75b3b
Ishchenko-Vladislav <patriq.968@gmail.com>
2022-06-23 19:31:38 +0300

    allsrc

--- src/allsrc/profile.jsx
+++ src/allsrc/profile.jsx
@@ -1,6 +1,6 @@
 function Profile() {
 	return <div className="main-profile">
-		<img className="main-profile-img" src="https://jpeg.org/images/jpegxt-home.jpg" alt="природа" />
+		<img className="main-profile-img" src="https://jpeg.org/images/jpegxt-home.jpg" alt="nature" />
 		<div>
 			<div><img className="main-profile-avatar" src="https://pngimg.com/uploads/deadpool/deadpool_PNG3.png" alt="" /></div>
 			<div>info</div>
